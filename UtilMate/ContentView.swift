9ae1092735cae7cedd61536777ca50e122db710e
HACO <jasonytonlinecomeandsee@gmail.com>
2023-09-14 07:44:55 +0800

    Initial Commit

---
+++ UtilMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UtilMate
+//
+//  Created by Haco on 2023/9/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UtilMate/UtilMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilMateApp.swift
+//  UtilMate
+//
+//  Created by Haco on 2023/9/14.
+//
+
+import SwiftUI
+
+@main
+struct UtilMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
